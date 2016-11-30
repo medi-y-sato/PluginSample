1c1f726a3cfb9bb1a7e97445f15d811c224250eb
Yoshiaki Satoh <y-sato@mediba.jp>
2016-11-30 18:49:59 +0900

    コンパイルエラーが鬱陶しいので未使用importなどを整理

--- src/pages/socialsharing/socialsharing.ts
+++ src/pages/socialsharing/socialsharing.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup } from '@angular/forms';
 import { SocialSharing } from 'ionic-native'
 /*
   Generated class for the Socialsharing page.
